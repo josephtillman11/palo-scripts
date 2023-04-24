398bb122d74a2993f489b16a8e9d8685fba0b4ea
Joseph Tillman <32916577+josephtillman11@users.noreply.github.com>
2023-04-24 15:46:07 -0400

    Update categoryLookup.py
    Create function

--- url-category-lookup/categoryLookup.py
+++ url-category-lookup/categoryLookup.py
@@ -9,21 +9,25 @@ host = Constants.PAN_HOST
 url = sys.argv[1]
 key = Constants.PAN_API_KEY
 
-r = requests.get(f'https://{host}/api/?type=op&cmd=<test><url>{url}</url></test>&key={key}', verify=True)
-root = ET.fromstring(r.text)
+def checkUrl (url):
+    r = requests.get(f'https://{host}/api/?type=op&cmd=<test><url>{url}</url></test>&key={key}', verify=True)
+    root = ET.fromstring(r.text)
 
-try:
-    ip_object = ipaddress.ip_address(url)
-    print(f"{url} is an IP address - doing reverse lookup...")
+    try:
+        ip_object = ipaddress.ip_address(url)
+        print(f"{url} is an IP address - doing reverse lookup...")
 
-    name, alias, addresslist = socket.gethostbyaddr(url)
-    print(f'Reverse lookup: {name}')
+        name, alias, addresslist = socket.gethostbyaddr(url)
+        print(f'Reverse lookup: {name}')
 
-except ValueError:
-    print(f"{url} is not an IP address, therefore reverse lookup wont be done.")
+    except ValueError:
+        print(f"{url} is not an IP address, therefore reverse lookup wont be done.")
 
-category = root[0].text.replace("\n", "")
-parsed = category.split(" ")
-category = parsed[-4] + " " + parsed [-3] + " " + parsed[-2]+ " " + parsed[-1]
+    category = root[0].text.replace("\n", "")
+    parsed = category.split(" ")
+    category = parsed[-4] + " " + parsed [-3] + " " + parsed[-2]+ " " + parsed[-1]
 
-print(f"The URL is categorized as: {category}")
+    print(f"The URL is categorized as: {category}")
+
+if __name__ == "__main__":
+    checkUrl(url)
