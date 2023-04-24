5987c12bbff9781bf6e1620627692d533ac29e90
Joseph Tillman <32916577+josephtillman11@users.noreply.github.com>
2023-04-24 15:41:32 -0400

    Create categoryLookup.py

---
+++ url-category-lookup/categoryLookup.py
@@ -0,0 +1,29 @@
+import sys
+import Constants
+import requests
+import xml.etree.ElementTree as ET
+import ipaddress,socket
+
+
+host = Constants.PAN_HOST
+url = sys.argv[1]
+key = Constants.PAN_API_KEY
+
+r = requests.get(f'https://{host}/api/?type=op&cmd=<test><url>{url}</url></test>&key={key}', verify=True)
+root = ET.fromstring(r.text)
+
+try:
+    ip_object = ipaddress.ip_address(url)
+    print(f"{url} is an IP address - doing reverse lookup...")
+
+    name, alias, addresslist = socket.gethostbyaddr(url)
+    print(f'Reverse lookup: {name}')
+
+except ValueError:
+    print(f"{url} is not an IP address, therefore reverse lookup wont be done.")
+
+category = root[0].text.replace("\n", "")
+parsed = category.split(" ")
+category = parsed[-4] + " " + parsed [-3] + " " + parsed[-2]+ " " + parsed[-1]
+
+print(f"The URL is categorized as: {category}")
